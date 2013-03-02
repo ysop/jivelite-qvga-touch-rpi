df4ce6ce1021d2832df0b91967c69c0d9cc5d4e3
Adrian Smith <triode1@btinternet.com>
2013-03-02 22:53:29 +0000

    make it build on fedora

--- src/common.h
+++ src/common.h
@@ -7,73 +7,25 @@
 #ifndef JIVE_COMMON_H
 #define JIVE_COMMON_H
 
-#include "config.h"
+#define HAVE_SOCKETPAIR
 
 #include <assert.h>
 #include <errno.h>
 #include <math.h>
 #include <signal.h>
 #include <stdio.h>
-
-#ifdef HAVE_DIRECT_H
-#include <direct.h>
-#endif
-
-#ifdef HAVE_DIRENT_H
-#include "dirent.h"
-#endif
-
-#ifdef HAVE_FCNTL_H
 #include <fcntl.h>
-#endif
-
-#ifdef HAVE_LIBGEN_H
 #include <libgen.h>
-#endif
-
-#ifdef HAVE_LIBPTHREAD
 #include <pthread.h>
-#endif
-
-#ifdef HAVE_STDLIB_H
 #include <stdlib.h>
-#endif
-
-#ifdef HAVE_STROPTS_H
-#include <stropts.h>
-#endif
-
-#ifdef HAVE_STRING_H
 #include <string.h>
-#endif
-
-#ifdef HAVE_SYS_TIME_H
 #include <sys/time.h>
-#endif
-
-#ifdef HAVE_SYS_SHM_H
 #include <sys/shm.h>
-#endif
-
-#ifdef HAVE_SYS_SOCKET_H
 #include <sys/socket.h>
-#endif
-
-#ifdef HAVE_SYS_UTSNAME_H
 #include <sys/utsname.h>
-#endif
-
-#ifdef HAVE_UNISTD_H
 #include <unistd.h>
-#endif
-
-#ifdef HAVE_NETDB_H
 #include <netdb.h>
-#endif
-
-#ifdef HAVE_ARPA_INET_H
 #include <arpa/inet.h>
-#endif
 
 
 #ifndef PATH_MAX
